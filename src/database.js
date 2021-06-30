3f07d1965a3574a365bce95016ea92f3fe71e04f
liquitycity <71210287+liquitycity@users.noreply.github.com>
2021-06-30 17:07:03 +0800

    Update database.js

--- src/database.js
+++ src/database.js
@@ -2,21 +2,26 @@ module.exports = {
   "1": {
     "name": "Electric Car",
     "description": "ThisElectric Car pays 60 Earn tokens every week to NFT holder."
+    "image": "https://yearncity.com/assets/img/ycar1500.png"
   },
   "2": {
     "name": "Sedan Car",
     "description": "ThisElectric Car pays 260 Earn tokens every week to NFT holder."
+    "image": "https://yearncity.com/assets/img/ycar2500.png"
   },
   "3": {
     "name": "Sports Car",
     "description": "ThisElectric Car pays 360 Earn tokens every week to NFT holder."
+    "image": "https://yearncity.com/assets/img/ycar3500.png"
   },
   "4": {
     "name": "Electric Car",
     "description": "ThisElectric Car pays 460 Earn tokens every week to NFT holder."
+    "image": "https://yearncity.com/assets/img/ycar4500.png"
   },
   "5": {
     "name": "Electric Car",
     "description": "ThisElectric Car pays 560 Earn tokens every week to NFT holder."
+    "image": "https://yearncity.com/assets/img/ybuss500.png"
   }
 }
