1a3a8cb48dc2bf2b4fabd6a5b97b90bfb014a7b8
liquitycity <71210287+liquitycity@users.noreply.github.com>
2021-06-30 15:25:52 +0800

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,22 +1,22 @@
 module.exports = {
   "1": {
-    "name": "Chunky Monkey",
-    "birthday": "10-18"
+    "name": "Electric Car",
+    "description": "ThisElectric Car pays 60 Earn tokens every week to NFT holder."
   },
   "2": {
-    "name": "Flerp Derp",
-    "birthday": "01-08"
+    "name": "Sedan Car",
+    "description": "ThisElectric Car pays 260 Earn tokens every week to NFT holder."
   },
   "3": {
-    "name": "Buttle McFuddle",
-    "birthday": "01-22"
+    "name": "Sports Car",
+    "description": "ThisElectric Car pays 360 Earn tokens every week to NFT holder."
   },
   "4": {
-    "name": "Quiero Pancakes",
-    "birthday": "03-04"
+    "name": "Electric Car",
+    "description": "ThisElectric Car pays 460 Earn tokens every week to NFT holder."
   },
   "5": {
-    "name": "Guino the Penguin",
-    "birthday": "03-09"
+    "name": "Electric Car",
+    "description": "ThisElectric Car pays 560 Earn tokens every week to NFT holder."
   }
 }
